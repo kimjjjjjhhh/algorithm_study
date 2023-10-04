14decb320c41c03aef26d9fb03a1a47a6dd51b30
kimjjjjjhhh <146559696+kimjjjjjhhh@users.noreply.github.com>
2023-10-04 22:07:21 +0900

    백준 1546번 c++

--- 1주차.cpp
+++ 1주차.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-int main() {
+/*int main() {
     int a;
     cin >> a;
     cin.ignore(); // Enter 키 입력을 무시하기 위해 추가
@@ -31,4 +31,19 @@ int main() {
     }
 
     return 0;
+}*/
+int main() {
+    int n;
+    cin >> n; // 시험 본 과목 개수를 받음
+    int sum=0; 
+    int max=0; // 시험 본 과목 중 가장 잘 본 점수를 저장
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        sum += a; // 평균을 구하기 위해 과목들의 점수를 다 더함
+        if (a >= max) {
+            max = a; // 가장 잘 본 점수를 if문을 사용해 골라냄
+        }
+    }
+    cout << sum * 100. / max / n;
 }
